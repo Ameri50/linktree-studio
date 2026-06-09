259f9d8b97b992022ad87283e5590aeb43f1976f
Moises Rojas <ssj52949@gmail.com>
2026-06-08 20:11:43 -0500

    qqqqqq

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-/* eslint-disable prettier/prettier */
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
     ssr: false,
   },
-  vite: {
-    plugins: [react()],
-  },
- 
 });
